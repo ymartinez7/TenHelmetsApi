114dcf5e205d22c3686e5cd5e29aa6340da84b70
Yansel Martinez <ymartinez7@gmail.com>
2019-11-14 15:07:26 +0100

    Se agregaron las entidades de construcción

---
+++ 10Helmets.API.Core/Entities/Inspection.cs
@@ -0,0 +1,35 @@
+﻿namespace _10Helmets.API.Core.Entities
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class Inspection : Base
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ActivityId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int InspectionTypeId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Activity Activity { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual InspectionType InspectionType { get; set; }
+    }
+}
---
+++ 10Helmets.API.Core/Entities/InspectionType.cs
@@ -0,0 +1,31 @@
+﻿namespace _10Helmets.API.Core.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class InspectionType : Base
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public InspectionType()
+        {
+            this.Inspections = new HashSet<Inspection>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IEnumerable<Inspection> Inspections { get; private set; }
+    }
+}
