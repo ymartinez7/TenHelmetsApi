8996a2c54db3bd3962b2c2a02082c21e0647a4d7
Yansel Martinez <ymartinez7@gmail.com>
2019-11-14 15:22:03 +0100

    Se agregaron a sentidades de recursos humanos

---
+++ 10Helmets.API.Core/Entities/DocumentType.cs
@@ -0,0 +1,33 @@
+﻿namespace _10Helmets.API.Core.Entities
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DocumentType : Base
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DocumentType()
+        {
+            this.Employees = new HashSet<Employee>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IEnumerable<Employee> Employees { get; private set; }
+    }
+}
