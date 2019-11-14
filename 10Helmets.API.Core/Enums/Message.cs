b79da4956827abfb7ff7c073fff6a93458b6d764
Yansel Martinez <ymartinez7@gmail.com>
2019-11-14 16:25:19 +0100

    Se agregó el DTO, las enumeraciones

---
+++ 10Helmets.API.Core/Enums/Message.cs
@@ -0,0 +1,33 @@
+﻿namespace _10Helmets.API.Core.Enums
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum Message : int
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Correct = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        RequestError,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        InternalError,
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        NullParameter,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        NotAuthorized,
+    }
+}
