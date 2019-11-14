f8391a1459ca85cd3bbc9288aee7783e6cc27e2b
Yansel Martinez <ymartinez7@gmail.com>
2019-11-14 15:05:21 +0100

    Se agregaron las entidades de administración
    Se incluyeron todas las clases de entidades correspondientes a
    administración

---
+++ 10Helmets.API.Core/Entities/EspenseType.cs
@@ -0,0 +1,33 @@
+﻿namespace _10Helmets.API.Core.Entities
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class EspenseType : Base
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public EspenseType()
+        {
+            this.Espenses = new HashSet<Espense>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual IEnumerable<Espense> Espenses { get; private set; }
+    }
+}
